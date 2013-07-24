3c33ad0c2fe2961a4edba6e7bfd67d50c3cedb15
John Edvard Reiten <johnedvard89@gmail.com>
2013-07-25 01:36:40 +0200

    organized imports

--- SpaceCommandes/src/com/rauma/lille/level002/Level002ActorStage.java
+++ SpaceCommandes/src/com/rauma/lille/level002/Level002ActorStage.java
@@ -2,15 +2,11 @@ package com.rauma.lille.level002;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.Group;
 import com.rauma.lille.Resource;
 import com.rauma.lille.SpaceGame;
 import com.rauma.lille.Utils;
